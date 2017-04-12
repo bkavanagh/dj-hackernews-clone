fbf6c53d71658f02cfa7c459fe8e343e588ad9a7
nagracks <nagracks@users.noreply.github.com>
2017-04-13 00:34:58 +0530

    Configure posts' urls

--- hn_clone/posts/urls.py
+++ hn_clone/posts/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from .views import (
+        IndexView, DetailView,
+        )
+
 
 urlpatterns = [
-        url(r'^$', TemplateView.as_view(template_name='posts/index.html'), name='index'),
+        # url(r'^$', TemplateView.as_view(template_name='posts/index.html'), name='index'),
+        url(r'^$', IndexView.as_view(), name='index'),
+        url(r'^(?P<id>[0-9]+)/detail/$', DetailView.as_view(), name='detail'),
         # url(r'^create$', ),
         # url(r'^view', ),
         ]
