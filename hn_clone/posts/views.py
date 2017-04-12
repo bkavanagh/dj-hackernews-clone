59070325a45483ff067f5b4988ebc074def773d0
nagracks <nagracks@users.noreply.github.com>
2017-04-13 00:34:38 +0530

    Write views

--- hn_clone/posts/views.py
+++ hn_clone/posts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import (
+            render, get_object_or_404,
+            )
+from django.views import generic
 
-# Create your views here.
+from .models import (
+            Post, Author, Tag
+            )
+
+
+class IndexView(generic.ListView):
+    # model = Post
+    template_name = 'posts/index.html'
+    context_object_name = 'posts_list'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'posts/detail.html'
+    context_object_name = 'post'
+
+    def get_queryset(self, id):
+        return get_object_or_404(Post, id=id)
